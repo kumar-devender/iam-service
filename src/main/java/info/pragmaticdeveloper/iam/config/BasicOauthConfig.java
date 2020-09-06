6e65cb296a1dc60715b458b55d4defd291b744f0
devender.kumar <devender.kumar@auto1.com>
2020-09-07 01:08:59 +0200

    Added in memory user

--- src/main/java/info/pragmaticdeveloper/iam/config/BasicOauthConfig.java
+++ src/main/java/info/pragmaticdeveloper/iam/config/BasicOauthConfig.java
@@ -1,13 +1,24 @@
 package info.pragmaticdeveloper.iam.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class BasicOauthConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -17,5 +28,16 @@ public class BasicOauthConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() {
+        UserDetails devender = User.builder()
+                .username("devender")
+                .password(passwordEncoder.encode("password"))
+                .roles("SOFTWARE_ENGINEER")
+                .build();
+        return new InMemoryUserDetailsManager(devender);
     }
 }
